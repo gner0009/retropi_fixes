0772b8b8b1beb8e754423023c6da1249c87b474b
gner0009 <greiner0009@gmail.com>
2016-09-24 13:39:16 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+
+#This script install all the required files in the correct loacations. #Retropi should be installed before executing this scirpt.
+
+
+#Install runcommand fix.
+##This overcopys a runcommmand with -append statement on execute commented out
+###Back up orginal runcommand.sh
+cp //opt/retropie/configs/all/   
+###Copy over modified 
+
+#Install controller changer
+##This swaps the retroarch.cfg between controller types.
+
